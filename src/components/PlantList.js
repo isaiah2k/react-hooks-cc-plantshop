00120226a2cf706a814b715331fb519cc97baf33
IsaiahMitchell <zaymgmt707@gmail.com>
2025-05-11 12:18:05 -0700

    updates PlantList.js

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,22 @@
-import React from "react";
-import PlantCard from "./PlantCard";
+import React, { useEffect, useState } from "react"
+import PlantCard from "./PlantCard"
 
 function PlantList() {
+  const [plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then(setPlants)
+  }, [])
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+    <ul className="cards">
+      {plants.map((plant) => (
+        <PlantCard key={plant.id} {...plant} />
+      ))}
+    </ul>
+  )
 }
 
-export default PlantList;
+export default PlantList
